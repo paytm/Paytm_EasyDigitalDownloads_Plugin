c163832ca460f329db8a9d61d1718892cf3ef2ea
Abhishek Awasthi <abhishek2.awasthi@paytm.com>
2021-05-24 15:19:03 +0530

    update jscheckout

--- edd-paytm-gateway/edd-paytm-gateway.php
+++ edd-paytm-gateway/edd-paytm-gateway.php
@@ -4,13 +4,14 @@ Plugin Name: Easy Digital Downloads - Paytm Gateway
 Plugin URL: http://easydigitaldownloads.com/extension/paytm-gateway
 Description: A paytm gateway for Easy Digital Downloads
 Version: 1.0
-Author: Pippin Williamson
-Author URI: http://pippinsplugins.com
-Contributors: mordauk
+Author: Paytm
+Author URI: https://developer.paytm.com/docs/
+Contributors: Paytm
 */
 
 // Don't forget to load the text domain here. Paytm text domain is pw_edd
-include('encdec_paytm.php');
+include('includes/PaytmHelper.php');
+include('includes/PaytmChecksum.php');
 
 // registers the gateway
 function pw_edd_register_gateway( $gateways ) {
@@ -27,44 +28,72 @@ function pw_edd_paytm_gateway_cc_form() {
 }
 add_action('edd_paytm_gateway_cc_form', 'pw_edd_paytm_gateway_cc_form');
 // add_action( 'edd_sample_gateway_cc_form', '__return_false' );
+/* 
+	* Get the transaction token
+	*/
+	function edd_blinkCheckoutSend($paramData = array(),$payment='')
+	{	
+		global $edd_options;
+		
+		$data=array();
+		if(!empty($paramData['amount']) && (int)$paramData['amount'] > 0)
+		{
+			/* body parameters */
+			$paytmParams["body"] = array(
+				"requestType" => "Payment",
+				"mid" => $edd_options['paytm_merchant_id'],
+				"websiteName" => $edd_options['paytm_website_name'],
+				"orderId" => $paramData['order_id'],
+				"callbackUrl" => get_site_url().'/?edd-listener=PAYTM_IPN&payment_id='.$payment,
+				"txnAmount" => array(
+					"value" => (int)$paramData['amount'],
+					"currency" => "INR",
+				),
+				"userInfo" => array(
+					"custId" => $paramData['cust_id'],
+				),
+			);
+			
+			$checksum = PaytmChecksum::generateSignature(json_encode($paytmParams["body"], JSON_UNESCAPED_SLASHES), $edd_options['paytm_mer_access_key']); 
+			
+			$paytmParams["head"] = array(
+				"signature"	=> $checksum
+			);
+			if(isset($edd_options['paytm_select_mode'])){
+				$mode = $edd_options['paytm_select_mode'];
+			}else{
+				$mode = 0;
+			}
+			/* prepare JSON string for request */
+			$post_data = json_encode($paytmParams, JSON_UNESCAPED_SLASHES);
 
+			$url = PaytmHelper::getPaytmURL(PaytmConstants::INITIATE_TRANSACTION_URL, $mode) . '?mid='.$paytmParams["body"]["mid"].'&orderId='.$paytmParams["body"]["orderId"];
+			
+			$res= PaytmHelper::executecUrl($url, $paytmParams);
+			if(!empty($res['body']['resultInfo']['resultStatus']) && $res['body']['resultInfo']['resultStatus'] == 'S'){
+				$data['txnToken']= $res['body']['txnToken'];
+			}
+			else
+			{
+				$data['txnToken']="";
+			}
+			/* $txntoken = json_encode($res); */
+		}
+		return $data;
+	}
 
 // processes the payment
 function pw_edd_process_payment( $purchase_data ) {
-
+	
 	global $edd_options;
-
 	/**********************************
 	* set transaction mode
 	**********************************/
-	/*	19751/17Jan2018	*/
-		/*if ( edd_is_test_mode() ) {
-			$paytm_redirect = 'https://pguat.paytm.com/oltp-web/processTransaction?'; 
-		} else {
-			if($edd_options['paytm_select_mode'] == '1'){
-				$paytm_redirect = 'https://secure.paytm.in/oltp-web/processTransaction?'; 
-			}else{
-				$paytm_redirect = 'https://pguat.paytm.com/oltp-web/processTransaction?'; 
-			} 
-		}*/
-
-		/*if ( edd_is_test_mode() ) {
-			$paytm_redirect = 'https://securegw-stage.paytm.in/theia/processTransaction?'; 
-		} else {
-			if($edd_options['paytm_select_mode'] == '1'){
-				$paytm_redirect = 'https://securegw.paytm.in/theia/processTransaction?'; 
-			}else{
-				$paytm_redirect = 'https://securegw-stage.paytm.in/theia/processTransaction?'; 
-			} 
-		}*/
 		$paytm_redirect = $edd_options['paytm_transaction_url']."?"; 
-	/*	19751/17Jan2018 end	*/
-
 
 	// check for any stored errors
 	$errors = edd_get_errors();
 	if ( ! $errors ) {
-
 		$purchase_summary = edd_get_purchase_summary( $purchase_data );
 
 		/****************************************
@@ -78,7 +107,7 @@ function pw_edd_process_payment( $purchase_data ) {
 			'purchase_key' => $purchase_data['purchase_key'],
 			'currency'     => $edd_options['currency'],
 			'downloads'    => $purchase_data['downloads'],
-                        'gateway'       => 'paytm',
+            'gateway'      => 'paytm',
 			'cart_details' => $purchase_data['cart_details'],
 			'user_info'    => $purchase_data['user_info'],
 			'status'       => 'pending'
@@ -88,37 +117,69 @@ function pw_edd_process_payment( $purchase_data ) {
 
 		// record the pending payment
 		$payment = edd_insert_payment( $payment );
+		$paramData = array('amount' => $purchase_data['price'], 'order_id' => $purchase_data['purchase_key'], 'cust_id' => $purchase_data['user_email']);
+		$data= edd_blinkCheckoutSend($paramData,$payment);
 
-		$merchant_payment_confirmed = false;
-                $secret_key = $edd_options['paytm_mer_access_key'];
-                $params = 	array(
-                                'REQUEST_TYPE' => 'DEFAULT',
-	    			'MID' => $edd_options['paytm_merchant_id'],
-	    			'TXN_AMOUNT' => $purchase_data['price'],
-    				'CHANNEL_ID' => "WEB",
-                                'INDUSTRY_TYPE_ID' => $edd_options['paytm_industry_type'],
-                                'WEBSITE' => $edd_options['paytm_website_name'],
-                                'CUST_ID' => $purchase_data['user_email'],
-                                'ORDER_ID'=> $purchase_data['purchase_key'],
-                                'EMAIL'=> $purchase_data['user_email'],
-                                
-                                 );
-                if($edd_options['paytm_callback']=='1')
-				{
-					$params['CALLBACK_URL']= get_site_url().'/?edd-listener=PAYTM_IPN&payment_id='.$payment;
+		if(!empty($data['txnToken'])){
+			wp_enqueue_style('paytmEddpayment', plugin_dir_url( __FILE__ ) . 'assets/css/paytm-payments.css', array(), '', '');
+			
+			
+			if(isset($edd_options['paytm_select_mode'])){
+				$mode = $edd_options['paytm_select_mode'];
+			}else{
+				$mode = 0;
+			}
+
+			$checkout_url = str_replace('MID',$edd_options['paytm_merchant_id'], PaytmHelper::getPaytmURL(PaytmConstants::CHECKOUT_JS_URL,$mode));
+			$wait_msg='<script type="application/javascript" crossorigin="anonymous" src="'.$checkout_url.'" onload="invokeBlinkCheckoutPopup();"></script><div id="paytm-pg-spinner" class="paytm-woopg-loader"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div><div class="bounce4"></div><div class="bounce5"></div><p class="loading-paytm">Loading Paytm...</p></div><div class="paytm-overlay paytm-woopg-loader"></div>';
+			get_header();
+			echo '<script type="text/javascript">
+				function invokeBlinkCheckoutPopup(){
+					console.log("method called");
+					var config = {
+						"root": "",
+						"flow": "DEFAULT",
+						"data": {
+						  "orderId": "'.$purchase_data['purchase_key'].'", 
+						  "token": "'.$data['txnToken'].'", 
+						  "tokenType": "TXN_TOKEN",
+						  "amount": "'.$purchase_data['price'].'"
+						},
+						"integration": {
+							"platform": "EasyDigitalDownloads",
+							"version": "'.EDD_VERSION.'|'.PaytmConstants::PLUGIN_VERSION.'"
+						},
+						"handler": {
+						  "notifyMerchant": function(eventName,data){
+							console.log("notifyMerchant handler function called");
+							if(eventName=="APP_CLOSED")
+							{
+								jQuery(".loading-paytm").hide();
+								jQuery("#paytm-pg-spinner").hide();
+								jQuery(".paytm-overlay").hide();
+								jQuery(".refresh-payment").show();
+								history.go(-1);
+							}
+						  } 
+						}
+					  };
+					  if(window.Paytm && window.Paytm.CheckoutJS){
+						  window.Paytm.CheckoutJS.onLoad(function excecuteAfterCompleteLoad() {
+							  window.Paytm.CheckoutJS.init(config).then(function onSuccess() {
+								  window.Paytm.CheckoutJS.invoke();
+							  }).catch(function onError(error){
+								  console.log("error => ",error);
+							  });
+						  });
+					  } 
 				}
-
-                $checksum = getChecksumFromArray($params, $secret_key);
-                $params['CHECKSUMHASH'] =  $checksum;
-                foreach($params as $key => $val){
-                    $submit_Params .= trim($key).'='.trim(urlencode($val)).'&';
-                }
-                
-                 $submit_Params  = substr($submit_Params, 0, -1);
-              
-                $request = $paytm_redirect.$submit_Params; 
-                 wp_redirect($request);         
-                exit();
+				jQuery(document).ready(function(){ jQuery(".re-invoke").on("click",function(){ window.Paytm.CheckoutJS.invoke(); return false; }); });
+				</script>'.$wait_msg.'';	
+				get_footer();
+				exit();
+		}else{
+			$fail = true; // errors were detected
+		}
 	} else {
 		$fail = true; // errors were detected
 	}
@@ -141,7 +202,7 @@ add_action( 'init', 'edd_listen_for_paytm_gateway_ipn' );
 
 
 function edd_process_paytm_gateway_ipn() {
-    global $edd_options;
+	global $edd_options;
 	
 	if ( isset( $_SERVER['REQUEST_METHOD'] ) && $_SERVER['REQUEST_METHOD'] != 'POST' ) {
             
@@ -167,37 +228,51 @@ function edd_process_paytm_gateway_ipn() {
                 $paramList = $_POST;
                 $order_amount = $_POST['TXNAMOUNT'];
                 
-						//  code by paytm team
-                $bool = "FALSE";
+				//  code by paytm team
+                $isValidChecksum = "FALSE";
                 $secret_key = $edd_options['paytm_mer_access_key'];
-                $bool = verifychecksum_e($paramList, $secret_key, $checksum_recv);
                 
-                if ($bool == "TRUE") {	   
-					
+                if(!empty($_POST['CHECKSUMHASH'])){
+				$post_checksum = $_POST['CHECKSUMHASH'];
+				unset($_POST['CHECKSUMHASH']);	
+				}else{
+					$post_checksum = "";
+				}
+				$isValidChecksum = PaytmChecksum::verifySignature($_POST, $secret_key, $post_checksum);
+				//if ($bool == "TRUE") {	   
+                if ($isValidChecksum == "TRUE") {	   
 					// Create an array having all required parameters for status query.
-					$requestParamList = array("MID" => $edd_options['paytm_merchant_id'] , "ORDERID" => $order_sent);
+					//$requestParamList = array("MID" => $edd_options['paytm_merchant_id'] , "ORDERID" => $order_sent);
 					
-					$StatusCheckSum = getChecksumFromArray($requestParamList, $secret_key);
+					//$StatusCheckSum = getChecksumFromArray($requestParamList, $secret_key);
 							
-					$requestParamList['CHECKSUMHASH'] = $StatusCheckSum;
+					//$requestParamList['CHECKSUMHASH'] = $StatusCheckSum;
 					
-					// Call the PG's getTxnStatus() function for verifying the transaction status.
-					/*	19751/17Jan2018	*/	
-						/*if($edd_options['paytm_select_mode'] == '1') {
-							$check_status_url = 'https://secure.paytm.in/oltp/HANDLER_INTERNAL/getTxnStatus';
-						} else {
-							$check_status_url = 'https://pguat.paytm.com/oltp/HANDLER_INTERNAL/getTxnStatus';
-						}*/
-
-						/*if($edd_options['paytm_select_mode'] == '1') {
-							$check_status_url = 'https://securegw.paytm.in/merchant-status/getTxnStatus';
-						} else {
-							$check_status_url = 'https://securegw-stage.paytm.in/merchant-status/getTxnStatus';
-						}*/
-						$check_status_url = $edd_options['paytm_transaction_status_url'];
+					//$check_status_url = $edd_options['paytm_transaction_status_url'];
 					/*	19751/17Jan2018 end	*/	
-					$responseParamList = callNewAPI($check_status_url, $requestParamList);	
-					if($responseParamList['STATUS']=='TXN_SUCCESS' && $responseParamList['TXNAMOUNT']==$_POST['TXNAMOUNT'])
+					//$responseParamList = callNewAPI($check_status_url, $requestParamList);	
+
+					$reqParams = array(
+						"MID" 		=> $edd_options['paytm_merchant_id'],
+						"ORDERID" 	=> $order_sent
+					);
+				
+					$reqParams['CHECKSUMHASH'] = PaytmChecksum::generateSignature($reqParams, $secret_key);
+
+					/* number of retries untill cURL gets success */
+					$retry = 1;
+					do{
+						if(isset($edd_options['paytm_select_mode'])){
+							$mode = $edd_options['paytm_select_mode'];
+						}else{
+							$mode = 0;
+						}
+						$resParams = PaytmHelper::executecUrl(PaytmHelper::getPaytmURL(PaytmConstants::ORDER_STATUS_URL,$mode), $reqParams);
+						$retry++;
+					} while(!$resParams['STATUS'] && $retry < PaytmConstants::MAX_RETRY_COUNT);
+					/* number of retries untill cURL gets success */
+
+					if($resParams['STATUS']=='TXN_SUCCESS' && $resParams['TXNAMOUNT']==$_POST['TXNAMOUNT'])
 					{
 						$payment_meta   = edd_get_payment_meta( $payment_id );
 						edd_insert_payment_note( $payment_id, sprintf( __( 'Thank you for your order . Your transaction has been successful. Paytm Transaction ID: %s', 'edd' ) , $_REQUEST['TXNID'] ) );
@@ -283,13 +358,13 @@ function pw_edd_add_settings( $settings ) {
 				'type' => 'text',
 				'size' => 'regular',
 			),
-			/*'paytm_select_mode' => array(
+			'paytm_select_mode' => array(
 				'id'   => 'paytm_select_mode',
 				'name' => __( 'Select Mode', 'pw_edd' ),
 				'desc' => __( '0 for stagging, 1 for Production', 'pw_edd' ),
 				'type' => 'text',
 				'size' => 'regular',
-			),*/
+			),
             'paytm_website_name' => array(
 				'id'   => 'paytm_website_name',
 				'name' => __( 'Website Name', 'pw_edd' ),
